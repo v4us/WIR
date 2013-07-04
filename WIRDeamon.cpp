30474019959b4452463ba571c11babffb7c58796
Nikolay Degtyarev <n.a.degtyarev@gmail.com>
2013-07-04 15:53:19 +0400

    server update

--- WIRDeamon.cpp
+++ WIRDeamon.cpp
@@ -7,7 +7,65 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <string.h>
+#include <sstream>
+#include "mongoose.h"
 #include "WIR01.h"
+
+#define MAX_NUMBER_OF_RESULTS 5
+using namespace std;
+
+template <class T>
+inline std::string toString (const T& t)
+{
+    std::stringstream ss;
+    ss << t;
+    return ss.str();
+}
+WIR01 classifier;
+
+static void noFoundReplay (struct mg_connection *conn)
+{
+    mg_printf(conn, "HTTP/1.1 404 Not Found\n");
+}
+// This function will be called by mongoose on every new request.
+static int begin_request_handler(struct mg_connection *conn) {
+  const struct mg_request_info *request_info = mg_get_request_info(conn);
+  if (strcmp(request_info->uri,"/image-process") == 0)
+  {
+      if (request_info->query_string == NULL)
+      {
+          noFoundReplay(conn);
+          return 1;
+      };
+        vector<WIRResult> results;
+        classifier.Recognize(request_info->query_string,results,MAX_NUMBER_OF_RESULTS);
+        char* content = new char[4096*results.size()];
+        if (content == NULL)
+        {
+            noFoundReplay(conn);
+            return 1;
+        }
+        // Prepare the message we're going to send
+        std::stringstream ss;
+        WIRResult::vectorOutput(ss,results);
+
+        // Send HTTP reply to the client
+        mg_printf(conn,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: application/json\r\n"
+                "Content-Length: %d\r\n"        // Always set Content-Length
+                "\r\n"
+                "%s",
+                ss.str().length, ss.str());
+  // Returning non-zero tells mongoose that our function has replied to
+  // the client, and mongoose should not send client any more data.
+    delete[] content;
+    return 1;
+  }
+  noFoundReplay(conn);
+  return 1;
+}
+
 
 int main(void) {
         
@@ -51,7 +109,18 @@ int main(void) {
         //close(STDERR_FILENO);
         
         /* Daemon-specific initialization goes here */
-        
+        struct mg_context *ctx;
+  struct mg_callbacks callbacks;
+
+  // List of options. Last element must be NULL.
+  const char *options[] = {"listening_ports", "8080", NULL};
+  classifier.loadTrainingDB("~/winee/WIR01/data/test_data.xml");
+  // Prepare callbacks structure. We have only one callback, the rest are NULL.
+  memset(&callbacks, 0, sizeof(callbacks));
+  callbacks.begin_request = begin_request_handler;
+
+  // Start the web server.
+  ctx = mg_start(&callbacks, NULL, options);
         /* The Big Loop */
         while (1) {
            /* Do some task here ... */
